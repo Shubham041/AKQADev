aa7cae7e5e20f6715711effb65036411c0ae605c
Shubham Prakash <shubham.prakash@nagarro.com>
2020-03-15 23:38:33 +0530

    First Commit

---
+++ AKQAProject/AKQAProject.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AKQAProject;
+using AKQAProject.Controllers;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AKQAProject.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void CheckIsNotNull()
+        {
+            HomeApiController controller = new HomeApiController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+            string result = controller.RetrunWords("XYZ", "123").StatusCode.ToString();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CheckValues()
+        {
+            HomeApiController controller = new HomeApiController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+            HttpResponseMessage result = controller.RetrunWords("XYZ", "123");
+            string strVal = result.Content.ReadAsStringAsync().Result;
+           Assert.AreEqual(strVal, "\"One Hundred and TwentyThree  Cent\"");
+        }
+
+        
+    }
+}
---
+++ AKQAProject/AKQAProject/Controllers/HomeApiController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace AKQAProject.Controllers
+{
+    public class HomeApiController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage RetrunWords(string name, string number)
+        {
+            string textOfNumber = string.Empty;
+            string isNegative = string.Empty;
+            try
+            {
+                number = Convert.ToDouble(number).ToString();
+
+                if (number.Contains("-"))
+                {
+                    isNegative = "Minus ";
+                    number = number.Substring(1, number.Length - 1);
+                }
+                if (number == "0")
+                {
+                    //Console.WriteLine("The number in currency fomat is \nZero Only");
+                }
+                else
+                {
+                    // Console.WriteLine("The number in currency fomat is \n{0}", isNegative +Helper. ConvertToWords(number));
+                    textOfNumber = Helper.ConvertToWords(number);
+                }
+                //Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            HttpResponseMessage response = Request.CreateResponse(
+                                            HttpStatusCode.OK, textOfNumber);
+            return response;
+           // return base.ResponseMessage(new HttpResponseMessage()
+           // {
+           //     Content =
+           //     new StringContent(
+           //textOfNumber, Encoding.UTF8, "text/html")
+           // });
+        }
+    }
+}
